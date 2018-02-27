aedd5e326e2600a8739a1f6cbbf23a50915e4d3e
Zachary Abresch <zachary.abresch@gmail.com>
2018-02-26 20:07:56 -0600

    Hook up inputs & `moment`
    * TDD!

--- src/components/Dur.js
+++ src/components/Dur.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import moment from "moment";
 import {
   Form,
   FormGroup,
@@ -10,37 +11,67 @@ import {
   Col
 } from "react-bootstrap";
 
-const Dur = () => (
-  <div>
-    <h1>Dur</h1>
-    <Grid>
-      <Form>
-        <Row>
-          <Col md={6}>
-            <FormGroup>
-              <ControlLabel>Start date: </ControlLabel>
-              <FormControl
-                type="text"
-                className="start-input"
-                placeholder="Start date & time"
-              />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <ControlLabel>End date: </ControlLabel>
-              <FormControl
-                type="text"
-                className="end-input"
-                placeholder="End date & time"
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Button className="calculate-btn">Calculate duration</Button>
-      </Form>
-    </Grid>
-  </div>
-);
+export class Dur extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment().format("L"),
+      endDate: moment()
+        .add(1, "days")
+        .format("L")
+    };
+  }
+
+  handleStartChange(event) {
+    this.setState({
+      startDate: event.target.value
+    });
+  }
+
+  handleEndChange(event) {
+    this.setState({
+      endDate: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dur</h1>
+        <Grid>
+          <Form>
+            <Row>
+              <Col md={6}>
+                <FormGroup>
+                  <ControlLabel>Start date: </ControlLabel>
+                  <FormControl
+                    type="text"
+                    className="start-input"
+                    placeholder="Start date & time"
+                    value={this.state.startDate}
+                    onChange={this.handleStartChange.bind(this)}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <ControlLabel>End date: </ControlLabel>
+                  <FormControl
+                    type="text"
+                    className="end-input"
+                    placeholder="End date & time"
+                    value={this.state.endDate}
+                    onChange={this.handleEndChange.bind(this)}
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Button className="calculate-btn">Calculate duration</Button>
+          </Form>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default Dur;
--- src/components/Dur.test.js
+++ src/components/Dur.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import moment from "moment";
 
 import Dur from "./Dur";
 
@@ -17,5 +18,37 @@ describe("Dur", () => {
   });
   it("contains a button to calculate", () => {
     expect(dur.find(".calculate-btn").length).toEqual(1);
+  });
+
+  it("has the initialState", () => {
+    const initialState = {
+      startDate: moment().format("L"),
+      endDate: moment()
+        .add(1, "days")
+        .format("L")
+    };
+    expect(dur.state()).toEqual(initialState);
+  });
+
+  describe("when typing into the `start-input`", () => {
+    beforeEach(() => {
+      dur
+        .find(".start-input")
+        .simulate("change", { target: { value: "01/01/2018" } });
+    });
+    it("sets the `startDate` in the `state`", () => {
+      expect(dur.state().startDate).toEqual("01/01/2018");
+    });
+  });
+
+  describe("when typing into the `end-input`", () => {
+    beforeEach(() => {
+      dur
+        .find(".end-input")
+        .simulate("change", { target: { value: "01/02/2018" } });
+    });
+    it("sets the `endDate` in the `state`", () => {
+      expect(dur.state().endDate).toEqual("01/02/2018");
+    });
   });
 });
